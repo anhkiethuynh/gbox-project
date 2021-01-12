9554a83b3cf1ac25923f4c30f9129bedd3e74f4c
akhuynh2535 <62603046+akhuynh2535@users.noreply.github.com>
2021-01-12 12:41:06 +0700

    html/css-completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(window).on("load", function(){
-   $('.svg').svgToInline();
-})
+$(window).on("load", function () {
+	$(".svg").svgToInline();
+});
+
+$(".btn-menu").on("click", function () {
+	if ($(this).hasClass("opened")) {
+		// console.log(true);
+		$(".menu").addClass("opened");
+	} else {
+		$(".menu").removeClass("opened");
+	}
+});
+
+$(".btn-top").on("click", function () {
+	$("html").animate({
+		scrollTop: 0,
+		scrollBehavior: "smooth",
+	});
+});
